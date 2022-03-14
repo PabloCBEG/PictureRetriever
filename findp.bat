b22fd5ea3be5b96097facca0b10c2d6f6eb60e74
PabloCBEG <pablocarben@outlook.com>
2022-03-14 11:16:51 +0100

    Programs for branch pruebas

--- findp.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-TITLE findp
-ECHO we'll find all images in this directory for you
-pause
--- retriever.py
+++
@@ -1,9 +0,0 @@
-#we'll also develop a Python option, since
-#we have not administrator permissions, hence
-#won't be able to compile (have no compiler installed),
-#nor execute programs
-
-import glob
-
-targetPattern = r"C:\Test\**\*.txt"
-glob.glob(targetPattern)
