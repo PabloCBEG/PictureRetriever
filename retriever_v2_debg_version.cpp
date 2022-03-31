c975fcff1cfd2abf9d981222e2b26dd48c373eec
PabloCBEG <pablocarben@outlook.com>
2022-03-31 09:26:38 +0200

    Final v2 commit

--- retriever_v2_debg_version.cpp
+++ retriever_v2_debg_version.cpp
@@ -63,24 +63,26 @@ void createfolder(fs::path& targetdirectory, fs::path& destinydirectory, fs::pat
 {
   //create a new folder for the pictures
 
-  std::string saux;                         //auxiliary string variable to store cin input
+  std::string saux; //auxiliary string variable to store cin input
   //Ask for target directory
   std::cout << "Introduzca la ruta completa del directorio donde quiere realizar la busqueda: ";
   std::getline(std::cin, saux);
-  targetdirectory = saux;                   //store target directory name in path cwd
+  targetdirectory = saux;                                 //store target directory name in path cwd
+  //cout << fs::path(targetdirectory) << endl;
 
   //ask for destiny directory
   std::cout << "Introduzca la ruta completa del directorio donde quiere almacenar las imagenes: ";
-  std::getline(std::cin, saux);             //just in case route contains blankspaces
+  std::getline(std::cin, saux);
   destinydirectory = saux;
 
-  aux = targetdirectory;                    //auxiliar variable for future operations
-  destinydirectory /= "todas_las_imagenes"; //append new folder name to current directory path
-  aux2 = destinydirectory;                  //save this path too, for the future (for cwd'll be later manipulated)
+  aux = targetdirectory;                                  //auxiliar variable for future operations
+  destinydirectory /= "todas_las_imagenes";                //append new folder name to current directory path
+  aux2 = destinydirectory;                                //save this path too, for the future (for cwd'll be later manipulated)
 
-  if(create_directory(destinydirectory) == -1)  //both create directory and check it was succesful //rmdir for removing
+  if(create_directory(destinydirectory) == -1)             //both create directory and check it was succesful //rmdir for removing
     cerr << "Error al crear el directorio: " << strerror(errno) << endl;
-  else ; //cout << "Directorio generado" << endl; //just for debugging
+  else ; cout << "Directorio generado" << endl; //just for debugging
+
 }
 
 void iteratefolder(std::vector<fs::path>& lista_archivos, fs::path& aux)
@@ -88,9 +90,8 @@ void iteratefolder(std::vector<fs::path>& lista_archivos, fs::path& aux)
   //iterates current dir; generates a path list
   //uses targetdirectory (aux must be targetdirectory)
 
-  recursive_directory_iterator it;  //variable of type rec.dir.iterator so it can be used as
-                                    //a "pointer" to the current directory when iterating
-  //searching for .jpg, .jpeg, .tiff, .png, .jfif, images, and videos
+  recursive_directory_iterator it;  //variable of type rec.dir.iterator so it can be used as a "pointer" to the current directory when iterating
+  //searching for .jpg images, extended to any image extension: .jpeg, .tiff, .png, .jfif
   for (const auto & file : it = recursive_directory_iterator(aux))
   {
     lista_archivos.push_back(file.path());  //adding new found file to the end of the vector
@@ -117,6 +118,8 @@ void iteratefolder(std::vector<fs::path>& lista_archivos, fs::path& aux)
     if(fs::current_path().filename() == "todas_las_imagenes") //not entering newly created folder, for I'd found duplicates
       it.disable_recursion_pending();
   }
+  //cout << "Todo bien" << endl;
+  //for(int i = 0; i < lista_archivos.size(); i++){ cout << fs::path(lista_archivos.at(i)) << endl;}
 }
 
 void filterlist(std::vector<fs::path>& lista_archivos, std::vector<fs::path>& lista_archivos_aux)
@@ -124,8 +127,8 @@ void filterlist(std::vector<fs::path>& lista_archivos, std::vector<fs::path>& li
   //filters list: removes repeated files comparing size and date of modification. Works for images and video files.
   //              different filtering criteria should be used for document files
 
-  uint32_t i, j;  //local indices for "for" loops
-  lista_archivos_aux = lista_archivos;  //auxiliary file list used for comparison and renaming
+  uint32_t i, j;
+  lista_archivos_aux = lista_archivos;
   for(i = 0; i < lista_archivos.size(); i++)
   {
     for(j = 0; j < lista_archivos.size(); j++)
@@ -133,10 +136,8 @@ void filterlist(std::vector<fs::path>& lista_archivos, std::vector<fs::path>& li
       if(lista_archivos.at(i).filename() == lista_archivos.at(j).filename() && i != j && lista_archivos_aux.at(i).filename() == lista_archivos_aux.at(j).filename()) //fs::path(lista_archivos.at(i)).compare(lista_archivos.at(j)) && i != j
       {
         //filtering condition
-          //sizes
         uint32_t tam1 = fs::file_size(lista_archivos.at(i));
         uint32_t tam2 = fs::file_size(lista_archivos.at(j));
-          //modification date (to the sec on Windows)
         fs::file_time_type dat1 = fs::last_write_time(lista_archivos.at(i));
         fs::file_time_type dat2 = fs::last_write_time(lista_archivos.at(j)); //fs::file_time_type
         if(tam1 == tam2)// || dat1 == dat2)
@@ -155,11 +156,14 @@ void filterlist(std::vector<fs::path>& lista_archivos, std::vector<fs::path>& li
                                 //i index in for loop
           fs::path termin = nuevo += ex;
           lista_archivos_aux.at(j).replace_extension();
+          //cout << lista_archivos.at(j).filename() << endl;
           lista_archivos_aux.at(j) += termin;
+          //cout << lista_archivos.at(j).filename() << endl;
         }
       }
     }
   }
+  for(i = 0; i < lista_archivos.size(); i++){ cout << fs::path(lista_archivos.at(i)) << endl; cout << fs::path(lista_archivos_aux.at(i)) << endl; }
 }
 
 void seekforimages(fs::path& destinydirectory, fs::path& aux2, std::vector<fs::path>& lista_archivos, std::vector<fs::path>& lista_archivos_aux)
@@ -167,10 +171,9 @@ void seekforimages(fs::path& destinydirectory, fs::path& aux2, std::vector<fs::p
   //looks for the images and COPIES them to the desired folder (new folder created)
   //uses destinydirectory: aux2 must be destinydirectory
 
-  uint32_t indice;  //index for looping file list
-  fs::path imagen;  //variable to store filename and append it to destiny directory route
+  uint32_t indice;
+  fs::path imagen;
 
-  //First of all, we filter the list to erase repeated files
   filterlist(lista_archivos, lista_archivos_aux);
 
   for(indice = 0; indice < lista_archivos.size(); indice++)
@@ -182,11 +185,7 @@ void seekforimages(fs::path& destinydirectory, fs::path& aux2, std::vector<fs::p
     // Show all errors concerning filesystem
     try
     {
-      fs::copy_file((const fs::path)lista_archivos.at(indice),
-                    destinydirectory /= imagen,
-                    copy_options::overwrite_existing);  //after filtering the list, any image
-                                                        //which, by chance, is repeated (probably
-                                                        //won't happen) shall be overwritten
+      fs::copy_file((const fs::path)lista_archivos.at(indice), destinydirectory /= imagen, copy_options::overwrite_existing);
     }
     catch(fs::filesystem_error& e)
     {
